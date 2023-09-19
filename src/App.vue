8a16c30d76263f1be930514e56b838ab1f780c77
Julie Lele <julielele16@gmail.com>
2023-09-19 08:54:58 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <Header title = "cool app" action ="get started" color="black" bgcolor="white"/>
+    </div>
+    <div class="container">
+        <Header title = "vue app" action ="todo list"/>
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+    name:'App',
+    components: {
+        Header
+    }
+}
+
+</script>
+
+<style>
+#app{
+    text-align: center;
+    margin-top: 60px;
+}
+.container{
+    border: 1px solid;
+    padding: 30px;
+    max-width: 500px;
+    margin: 20px auto;
+}
+button{
+    padding: 10px 20px;
+    font-size: 15px;
+    background: burlywood;
+    color: blueviolet;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+
+
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1 :style="{color: color}">{{ title }}</h1>
+        <button @click="handleclick()" :style="{background: bgcolor}">{{ action }}</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title : String,
+        action: String,
+        color: String,
+        bgcolor: String
+    },
+    methods: {
+        handleclick() {
+            console.log("button clicked");
+
+        }
+    }
+}
+
+</script>
+
+<style>
+button{
+    padding: 10px 20px;
+    font-size: 15px;
+    background: burlywood;
+    color: blueviolet;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+h1{
+        color: rgb(25, 171, 86);
+}
+</style>
